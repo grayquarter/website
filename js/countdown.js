bfdebca73b0d30e9a45b65b21907a9f7cd9d3cd8
grayquarter <john@grayquarter.com>
2017-02-17 00:00:19 -0800

    updates

--- js/countdown.js
+++ js/countdown.js
@@ -4,7 +4,7 @@
 		thisEl = $(this);
 		//array of custom settings
 		var settings = { 
-			'date': "3/13/2017",
+			'date': "13 February 2017 12:00:00",
 			'format': "on"
 		};
 		//append the settings array to options
