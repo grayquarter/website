373a221f6641fad95d6bf011aee23de2e7292521
grayquarter <john@grayquarter.com>
2017-04-08 23:40:16 -0700

    new site
    new site

---
+++ js/inspinia.js
@@ -0,0 +1,53 @@
+// INSPINIA Landing Page Custom scripts
+$(document).ready(function () {
+
+    // Highlight the top nav as scrolling
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 80
+    })
+
+    // Page scrolling feature
+    $('a.page-scroll').bind('click', function(event) {
+        var link = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(link.attr('href')).offset().top - 70
+        }, 500);
+        event.preventDefault();
+    });
+
+});
+
+// Activate WOW.js plugin for animation on scrol
+new WOW().init();
+
+
+////////////Back to top//////
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+});
